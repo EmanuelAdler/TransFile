fb0bff3b1eaf208006f69ac92677939f10142fe1
Andreferro <andresantanaferro10@gmail.com>
2017-05-22 21:33:29 -0300

    Update server file

--- src/ServerSide/TCPServer.java
+++ src/ServerSide/TCPServer.java
@@ -1,38 +1,32 @@
-package ServerSide;
-import java.io.*;
 import java.net.*;
+import java.io.*;
 
-class TCPServer{
-    public static void main(String argv[]) throws Exception{
+public class Main {
+
+    public static void main (String[] args ) throws IOException {
+
         int bytesRead;
         int current = 0;
 
-        ServerSocket serverSocket;
+        ServerSocket serverSocket = null;
         serverSocket = new ServerSocket(13267);
 
         while(true) {
-            Socket clientSocket;
+            Socket clientSocket = null;
             clientSocket = serverSocket.accept();
 
             InputStream in = clientSocket.getInputStream();
 
-            DataInputStream clientData = new DataInputStream(in);
+            // Writing the file to disk
+            // Instantiating a new output stream object
+            OutputStream output = new FileOutputStream("teste.txt");
 
-            String fileName = clientData.readUTF();
-            OutputStream output = new FileOutputStream(fileName);
-            long size = clientData.readLong();
             byte[] buffer = new byte[1024];
-            while (size > 0 && (bytesRead = clientData.read(buffer, 0, (int)Math.min(buffer.length, size))) != -1)
-            {
+            while ((bytesRead = in.read(buffer)) != -1) {
                 output.write(buffer, 0, bytesRead);
-                size -= bytesRead;
             }
-
             // Closing the FileOutputStream handle
-            in.close();
-            clientData.close();
             output.close();
         }
-
     }
 }
